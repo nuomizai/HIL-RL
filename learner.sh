c2c0a642760e03c09f7329d4b88c06e18c4b66e5
oyeey <1684308419@qq.com>
2026-01-08 19:25:56 +0800

    first commit

--- learner.sh
+++ learner.sh
@@ -1,9 +1,9 @@
 
-export PYTHONPATH=$PYTHONPATH:../../../lerobot/src/
-export PYTHONPATH=$PYTHONPATH:../../../RL-Robot-Env/
+export PYTHONPATH=$PYTHONPATH:../../lerobot/src/
+# export PYTHONPATH=$PYTHONPATH:../../../RL-Robot-Env/
 export PYTHONPATH=$PYTHONPATH:../../../HIL-RL
 export PYTHONPATH=$PYTHONPATH:/home/eai/Dev/sysEAI/xRocs/xRocs
-export http_proxy=http://127.0.0.1:8889 && export https_proxy=http://127.0.0.1:8889
+export http_proxy=http://127.0.0.1:7890 && export https_proxy=http://127.0.0.1:7890
 
 
 task_name=close_trashbin_franka_1028
